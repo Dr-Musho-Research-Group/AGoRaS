cad2875fd9d3d2a65c3a125532324f0dc0f690b1
rtempke <35894061+rtempke@users.noreply.github.com>
2021-06-27 12:53:32 -0400

    Update experiment2_VAE_reactions.py
    cleaning up

--- Architectures/experiment2_VAE_reactions.py
+++ Architectures/experiment2_VAE_reactions.py
@@ -17,7 +17,6 @@ print(kerasBKED)
 from keras.optimizers import Adam
 from keras.models import Model, Sequential, Input
 from keras.layers import Bidirectional, Dense, Embedding, Input, Lambda, LSTM, RepeatVector, TimeDistributed, Layer, Activation, Dropout
-# from keras.layers import Concatenate, Dropout, BatchNormalization
 import tensorflow as tf
 from keras import backend as K
 import pandas as pd
@@ -26,19 +25,8 @@ import tensorflow_addons as tfa
 from keras.preprocessing.sequence import pad_sequences
 from scipy import spatial
 
-# physical_devices = tf.config.list_physical_devices('GPU')
 tf.compat.v1.disable_eager_execution()
-# tf.config.experimental.set_memory_growth(physical_devices, True)
-# tf.compat.v1.estimator.tpu.TPUEstimator(use_tpu=True, eval_batch_size= 100)
 
-# BATCH_SIZE = 10
-# TRAINING_RATIO = 5
-# GRADIENT_PENALTY_WEIGHT = 1
-
-
-# Load data
-# with open('/nfs/home/6/tempker/GAN/Dataset/pkls/reactions-training-data.pkl', 'rb') as f: 
-#     train_data, wrd2ind, ind2wrd, vocabulary = pk.load(f)
 data_GAN = open('/nfs/home/6/tempker/GAN/Dataset/pkls/Balanced_original_equations_training_sequence_prebalanced_equations__1212020.pkl', 'rb')
 
 data_GAN = pk.load(data_GAN)
@@ -55,13 +43,6 @@ np.random.shuffle(train_data)
 
 num_eqs = train_data.shape[0]
 
-# test_split = 0.2
-
-# num_test_samples = math.ceil(num_eqs * test_split)
-
-# test = train_data[:num_test_samples].astype(np.int32)
-# training = train_data[num_test_samples:].astype(np.int32)
-
 training = train_data[:5900].astype(np.int32)
 test = train_data[5900:6900].astype(np.int32)
 
@@ -69,7 +50,6 @@ test = train_data[5900:6900].astype(np.int32)
 
 batch_size = 25
 max_len = len(train_data[1])
-# emb_dim = 250
 latent_dim = 350
 intermediate_dim = 500
 epsilon_std = 0.1
@@ -83,11 +63,6 @@ learnr = 1e-5
 x = Input(shape=(max_len,))
 x_embed = Embedding(nb_letters, intermediate_dim, input_length=max_len)(x)
 h = Bidirectional(LSTM(intermediate_dim, return_sequences=False, recurrent_dropout=0.2), merge_mode='concat')(x_embed)
-#h = Bidirectional(LSTM(intermediate_dim, return_sequences=False), merge_mode='concat')(h)
-#h = Dropout(0.2)(h)
-#h = (intermediate_dim, activation='linear')(h)
-#h = act(h)
-#h = Dropout(0.2)(h)
 z_mean = Dense(latent_dim)(h)
 z_log_var = Dense(latent_dim)(h)
 
@@ -97,9 +72,7 @@ def sampling(args):
                               stddev=epsilon_std)
     return z_mean + K.exp(z_log_var / 2) * epsilon
 
-# note that "output_shape" isn't necessary with the TensorFlow backend
 z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
-# we instantiate these layers separately so as to reuse them later
 repeated_context = RepeatVector(max_len)
 decoder_h = LSTM(intermediate_dim, return_sequences=True, recurrent_dropout=0.2)
 decoder_mean = Dense(nb_letters, activation='linear')#softmax is applied in the seq2seqloss by tf #TimeDistributed()
@@ -120,13 +93,11 @@ class CustomVariationalLayer(Layer):
         self.target_weights = tf.constant(np.ones((batch_size, max_len)), tf.float32)
 
     def vae_loss(self, x, x_decoded_mean):
-        #xent_loss = K.sum(metrics.categorical_crossentropy(x, x_decoded_mean), axis=-1)
         labels = tf.cast(x, tf.int32)
         xent_loss = K.sum(tfa.seq2seq.sequence_loss(x_decoded_mean, labels, 
                                                      weights=self.target_weights,
                                                      average_across_timesteps=False,
                                                      average_across_batch=False), axis=-1)#,
-                                                     #softmax_loss_function=softmax_loss_f), axis=-1)#,
         kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
         xent_loss = K.mean(xent_loss)
         kl_loss = K.mean(kl_loss)
@@ -138,7 +109,6 @@ class CustomVariationalLayer(Layer):
         print(x.shape, x_decoded_mean.shape)
         loss = self.vae_loss(x, x_decoded_mean)
         self.add_loss(loss, inputs=inputs)
-        # we don't use this output, but it has to have the correct shape:
         return K.ones_like(x)
     
 def kl_loss(x, x_decoded_mean):
@@ -172,19 +142,11 @@ vae.fit(training, training,
       batch_size=batch_size,
       validation_data=(test, test), callbacks=[checkpointer])
 
-
-
-# vae.fit(training, training,
-#      epochs=100,
-#      batch_size=batch_size)
-
-
 print(K.eval(vae.optimizer.lr))
 K.set_value(vae.optimizer.lr, learnr)
 
 
 
-# vae.save('models/vae_lstm_sample50_exp1.h5')
 vae.save_weights('models/vae_balanced_data_run_2.h5')
 #vae.load_weights('models/vae_seq2seq_test.h5')
 # build a model to project inputs on the latent space
@@ -218,18 +180,6 @@ print(''.join(original_sent))
 
 
 
-"""
-FIX THIS WITH IMPORTING THE TK FROM BEFORE
-"""
-
-#=================== Sentence processing and interpolation ======================#
-# function to parse a sentence
-# def sent_parse(sentence, mat_shape):
-#     sequence = tokenizer.texts_to_sequences(sentence)
-#     # pad_sequences(train_sequences, maxlen=None, padding='post', value = 49)
-#     padded_sent = pad_sequences(sequence, maxlen=max_len, padding='post', value = 49)
-#     return padded_sent#[padded_sent, sent_one_hot]
-
 # function to parse a sentence
 def sent_parse(sentence):
     sequence = tokenizer.texts_to_sequences(sentence)
@@ -249,8 +199,6 @@ def find_similar_encoding(sent_vect):
     return new_vec
 
 
-# input: two points, integer n
-# output: n equidistant points on the line between the input points (inclusive)
 def shortest_homology(point_one, point_two, num):
     dist_vec = point_two - point_one
     sample = np.linspace(0, 1, num, endpoint = True)
@@ -260,9 +208,6 @@ def shortest_homology(point_one, point_two, num):
     return hom_sample
 
 
-
-# input: original dimension sentence vector
-# output: sentence text
 def save_latent_sentence(sent_vect):
     sent_vect = np.reshape(sent_vect,[1,latent_dim])
     sent_reconstructed = generator.predict(sent_vect)
@@ -320,26 +265,6 @@ print('-----------------')
 
 new_sents_interp(sentence1, sentence2, 5)
         
-# check = [index2word[x] for x in mysent.tolist()]
-
-
-
-
-# gen = []
-
-# while len(gen) < 500000:
-#     if len(gen) < 500000:
-#         num1 = randint(0, 32057)
-#         num2 = randint(0, 32057)
-#         sent1 = train_data[num1].reshape(1,len(train_data[0]))
-#         sent2 = train_data[num2].reshape(1,len(train_data[0]))
-#         newintrp =  new_sents_generation(sent1, sent2, 10)
-#     gen.extend(newintrp)
-#     # gen = list(map(''.join,gen))
-#     # gen = [''.join(x) for x in gen]
-    
-    
-
 gen = []
 delete_list = ['~~','>>','= ', '[[', ']]', '> >', '[]','()','[ ', '[)', '(]', '~ ~', '\ \ ',
                '###', '..', '==', '# ', '((', '))']
